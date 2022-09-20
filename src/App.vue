814f17347f0ccbec348a78fafdedc59a7b546950
KarnesTH <p_haehnel@hotmail.de>
2022-09-20 12:11:06 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TheTwoColumnsLayout
+    :leftColumnClass="'col-md-8 bg-vue'"
+    :rightColumnClass="'col-md-4 bg-vue2'"
+    :fullsize="true"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TheTwoColumnsLayout from "./layouts/TheTwoColumnsLayout.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TheTwoColumnsLayout,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "~bootstrap/dist/css/bootstrap.min.css";
+@import "~@fortawesome/fontawesome-free/css/all.min.css";
+@import "~animate.css/animate.min.css";
+
+.bg-vue {
+  background-color: rgb(52, 73, 94);
+  color: #f3f3f3;
+}
+
+.bg-vue2 {
+  background-color: rgb(65, 184, 131);
+  color: #f3f3f3;
+}
+
+.text-vue {
+  color: rgb(52, 73, 94);
+}
+
+.text-vue2 {
+  color: rgb(65, 184, 131);
 }
 </style>
